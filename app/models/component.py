e1534a29ff88c85cd3b41a307e8cacd2bd26bde5
Joaquin Struyas <joacostruyas@gmail.com>
2025-11-05 19:20:32 -0300

    folder organization

---
+++ app/models/component.py
@@ -0,0 +1,9 @@
+class Component:
+    def __init__(self, id, name, batch, expiration, state, amount, total):
+        self.id = id
+        self.name = name
+        self.batch = batch
+        self.expiration = expiration
+        self.state = state
+        self.amount = amount
+        self.total = total
